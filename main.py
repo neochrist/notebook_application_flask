f148dcf93361a9c4801143ab9e894e566f3b4bf4
neochrist <kikouchia58@gmail.com>
2023-01-03 15:13:16 +0400

    added surname route

--- main.py
+++ main.py
@@ -29,12 +29,12 @@ def create_person():
       data = request.get_json()
       name = data["name"]
       surname = data["surname"]
-      # phone_number = data["phone_number"]
-      # description = data["description"]
+      phone_number = data["phone_number"]
+      description = data["description"]
       
       # Insert the new person into the database
-      sql = "INSERT INTO persons (name, surname) VALUES (%s, %s)"
-      cursor.execute(sql, (name, surname))
+      sql = "INSERT INTO persons (name, surname, phone_number, description) VALUES (%s, %s, %s, %s)"
+      cursor.execute(sql, (name, surname, phone_number, description))
       connection.commit()
       return "Person created successfully"
   except Exception as e:
@@ -52,8 +52,20 @@ def get_persons_by_name(name):
       return jsonify(result)
   except Exception as e:
     return str(e)
+  
+  # Create a route to get all persons with the same surname
+@app.route("/persons/surname/<surname>", methods=["GET"])
+def get_persons_by_name(name):
+  try:
+    with connection.cursor() as cursor:
+      # Select all persons with the specified name from the database
+      sql = "SELECT * FROM persons WHERE surname = %s"
+      cursor.execute(sql, name)
+      result = cursor.fetchall()
+      return jsonify(result)
+  except Exception as e:
+    return str(e)
 
-# Create a route to get all persons with the same surname
 if __name__ == "__main__":
   app.run()
 
